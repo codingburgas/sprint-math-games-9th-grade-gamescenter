884f8cbc26d7a162552164ea62829aa957c70a60
PGTenev24 <PGTenev24@codingburgas.bg>
2025-12-03 22:45:04 +0200

    Add Main Menu

--- GamesCenter/PL/main.cpp
+++ GamesCenter/PL/main.cpp
@@ -1,44 +1,86 @@
 ﻿#include "raylib.h"
+#include <vector>
+#include <string>
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+struct MenuOption {
+    std::string label;
+    bool selected;
+    Color currentColor;
+};
+
+Color LerpColor(Color a, Color b, float t) {
+    return {
+        (unsigned char)(a.r + (b.r - a.r) * t),
+        (unsigned char)(a.g + (b.g - a.g) * t),
+        (unsigned char)(a.b + (b.b - a.b) * t),
+        (unsigned char)(a.a + (b.a - a.a) * t)
+    };
+}
+
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Raylib Stylish Menu");
+    SetTargetFPS(60);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    std::vector<MenuOption> options = {
+        {"Games", false, WHITE},
+        {"How to Play", false, WHITE},
+        {"About Us", false, WHITE},
+        {"Exit", false, WHITE}
+    };
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        Vector2 mousePos = GetMousePosition();
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        for (int i = 0; i < options.size(); i++) {
+            Rectangle button = { 250, 200 + i * 90, 300, 70 };
+            bool hovered = CheckCollisionPointRec(mousePos, button);
+            Color targetColor = hovered ? ORANGE : WHITE;
+            options[i].currentColor = LerpColor(options[i].currentColor, targetColor, 0.1f);
+
+            if (hovered && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                options[i].selected = true;
+            }
+        }
+
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        for (int y = 0; y < screenHeight; y++) {
+            Color rowColor = Color{ (unsigned char)(20 + y / 15), (unsigned char)(40 + y / 20), 100, 255 };
+            DrawLine(0, y, screenWidth, y, rowColor);
+        }
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Game Menu", screenWidth / 2 - MeasureText("Game Menu", 50) / 2, 80, 50, DARKBLUE);
+
+        for (int i = 0; i < options.size(); i++) {
+            Rectangle button = { 250, 200 + i * 90, 300, 70 };
+            DrawRectangleRounded(button, 0.3f, 10, options[i].currentColor);
+
+            int textWidth = MeasureText(options[i].label.c_str(), 28);
+            DrawText(options[i].label.c_str(),
+                button.x + (button.width - textWidth) / 2,
+                button.y + 20,
+                28,
+                BLACK);
+        }
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
+
+        if (options[0].selected) {
+        }
+        if (options[1].selected) {
+        }
+        if (options[2].selected) {
+        }
+        if (options[3].selected) {
+            CloseWindow();
+        }
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
